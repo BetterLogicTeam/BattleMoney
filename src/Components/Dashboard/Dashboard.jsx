3b520436dee67b00841ed9465475f2970592ac11
Haider <haideralirafeeq@gmail.com>
2023-09-08 17:55:38 +0500

    after haider changes

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -47,6 +47,14 @@ import { BiUser } from "react-icons/bi";
 import Dropdown from "react-bootstrap/Dropdown";
 import Vertical from '../Vertical/Vertical';
 import Vertical2 from '../Vertical2/Vertical2';
+import { IoIosTennisball } from "react-icons/io";
+import { FaBasketballBall } from "react-icons/fa";
+import { MdSportsCricket } from "react-icons/md";
+import { BiFootball } from "react-icons/bi";
+import DashboardMatch_Slider from "../DashboardMatch_Slider/DashboardMatch_Slider";
+import DashboardMatchDetail_Tab from "../DashboardMatchDetail_Tab/DashboardMatchDetail_Tab";
+
+
 
 //import io from "socket.io-client";
 
@@ -3497,250 +3505,61 @@ function Dashboard() {
                   </div>
                 </div>
 
-                <div className=" tab-content_bg">
-                  <ul className="nav nav-pills" role="tablist">
-                    {/* {events_Data.slice(0, 3).map((items, index) => {
-                      return (
-                        <>
-                          <li className="nav-item" key={index}>
-                            <a
-                              href=""
-                              className="nav-link"
-                              data-toggle="pill"
-                              onClick={() => (
-                                eventCatagory(items.eventType),
-                                Current_Match(items.eventType),
-                                setevent_Type(items.eventType),
-                                setevent_name(items.name),
-                                navigate(
-                                  `/Dashboard/${items.name}/${items.eventType}`
-                                )
-                              )}
-                            >
-                              {items.name}
-                            </a>
-                          </li>
-                        </>
-                      );
-                    })} */}
-                  </ul>
-                  {/* <!-----tab pane------>  */}
-                  <div className="tab-content">
-                    <div className="tab-content">
-                      <div id="Home3" className="container tab-pane active">
-                        <div className="">
-                          <div className="table-responsive-sm">
-                            <table className="table">
-                              <thead>
-                                <tr>
-                                  {/* <th scope="col">#</th>
-                                  <th scope="col">id</th> */}
-                                  <th scope="col">Name</th>
-                                  <th scope="col">OpenDate</th>
-                                  <th scope="col"></th>
-                                  <th scope="col" className="temp"></th>
-                                  <th scope="col" className="temp">
-                                    1
-                                  </th>
-                                  <th scope="col" className="temp">
-                                    X
-                                  </th>
-                                  <th scope="col" className="temp">
-                                    2
-                                  </th>
-                                </tr>
-                              </thead>
-                              <tbody className="text-white">
-                                {eventCatagory_Data?.map((item, index) => (
-                                  <tr>
-                                    {/* <th scope="row">{index + 1}</th>
-                                    <td>{item.id}</td> */}
-                                    <td>
-                                      <a
-                                        href=""
-                                        style={{
-                                          color: "#fff",
-                                          textDecoration: "none",
-                                          textAlign: "center",
-                                        }}
-                                        onClick={() =>
-                                          navigate(
-                                            event_name === "Tennis"
-                                              ? `/Tennis_Matches?Id=${item.id}&Time=${item.openDate}`
-                                              : event_name === "Soccer"
-                                              ? `/Football_Matches?Id=${item.id}&Time=${item.openDate}`
-                                              : `/Live_Matches?Id=${item.id}&Time=${item.openDate}`
-                                          )
-                                        }
-                                      >
-                                        {item.name}
-                                      </a>
-                                    </td>
-                                    <td>
-                                      {moment(item.openDate).format(
-                                        "DD/MM/YYYY h:m:s A"
-                                      )}
-                                    </td>
-                                    <th scope="row">
-                                      {new Date(item.openDate) <=
-                                        new Date() && (
-                                        <span className="circle"> </span>
-                                      )}
-                                    </th>
-                                    <td>
-                                      <img src={tv} alt="" width="25" />
-                                    </td>
-                                    <th className="temp">
-                                      <span className="back_space">-</span>
-                                      <span className="back_space1">-</span>
-                                    </th>
-                                    <th className="temp">
-                                      <span className="back_space">-</span>
-                                      <span className="back_space1">-</span>
-                                    </th>
-                                    <td className="temp">
-                                      <span className="back_space">-</span>
-                                      <span className="back_space1">-</span>
-                                    </td>
-                                  </tr>
-                                ))}
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
+                <div className="second_bar_main">
+                  <div className="dv1">
+                    <a href="" className="my-3 dv1Inner d-flex justify-content-center align-items-center gap-2">
+                      <div className="icn"><IoIosTennisball className="mnicn"/></div>
+                      <div>
+                        <p className="mb-0 mchDtl">US Open <br /> Women 2023</p>
                       </div>
-                    </div>
-
-                    <div
-                      id="Home3"
-                      class="container tab-pane active"
-                      style={{ display: "none" }}
-                    >
-                      <div class="">
-                        <div class="table-responsive-sm">
-                          <table class="table">
-                            <thead>
-                              <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">id</th>
-                                <th scope="col">Name</th>
-                              </tr>
-                            </thead>
-                            <tbody className="text-white">
-                              {eventCatagory_Data?.map((item, index) => (
-                                <tr>
-                                  <th scope="row">{index + 1}</th>
-                                  <td>{item.id}</td>
-                                  <td>
-                                    <a
-                                      href=""
-                                      style={{
-                                        color: "#fff",
-                                        textDecoration: "none",
-                                      }}
-                                      onClick={() =>
-                                        navigate(
-                                          event_name === "Tennis"
-                                            ? `/Tennis/${item.id}/${event_Type}`
-                                            : event_name === "Soccer"
-                                            ? `/Soccer/${item.id}/${event_Type}`
-                                            : `/scoreboard/${item.id}/${event_Type}`
-                                        )
-                                      }
-                                    >
-                                      {item.name}
-                                    </a>
-                                  </td>
-                                </tr>
-                              ))}
-                            </tbody>
-                          </table>
-                        </div>
+                    </a>
+                  </div>
+                  <div className="dv1">
+                    <a href="" className="my-3 dv1Inner d-flex justify-content-center align-items-center gap-2">
+                      <div className="icn"><IoIosTennisball className="mnicn"/></div>
+                      <div>
+                        <p className="mb-0 mchDtl">US Open <br /> Men 2023</p>
                       </div>
-                    </div>
-                    <div id="Home4" class="container tab-pane fade">
-                      <br />
-                      <h3>Home4</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home5" class="container tab-pane fade">
-                      <br />
-                      <h3>Home5</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home6" class="container tab-pane fade">
-                      <br />
-                      <h3>Home6</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home7" class="container tab-pane fade">
-                      <br />
-                      <h3>Home7</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home8" class="container tab-pane fade">
-                      <br />
-                      <h3>Home8</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home9" class="container tab-pane fade">
-                      <br />
-                      <h3>Home9</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home10" class="container tab-pane fade">
-                      <br />
-                      <h3>Home10</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home11" class="container tab-pane fade">
-                      <br />
-                      <h3>Home11</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
-                    <div id="Home12" class="container tab-pane fade">
-                      <br />
-                      <h3>Home12</h3>
-                      <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam.
-                      </p>
-                    </div>
+                    </a>
+                  </div>
+                  <div className="dv1 basketball">
+                    <a href="" className="my-3 dv1Inner d-flex justify-content-center align-items-center gap-2">
+                      <div className="icn"><FaBasketballBall className="mnicn"/></div>
+                      <div>
+                        <p className="mb-0 mchDtl">FIBA World <br /> Cup 2023</p>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="dv1 cricket">
+                    <a href="" className="my-3 dv1Inner d-flex justify-content-center align-items-center gap-2">
+                      <div className="icn"><MdSportsCricket className="mnicn"/></div>
+                      <div>
+                        <p className="mb-0 mchDtl">Pakistan <br /> Women 2023</p>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="dv1 football">
+                    <a href="" className="my-3 dv1Inner d-flex justify-content-center align-items-center gap-2">
+                      <div className="icn"><BiFootball className="mnicn"/></div>
+                      <div>
+                        <p className="mb-0 mchDtl">Georgia vs <br /> Spain</p>
+                      </div>
+                    </a>
                   </div>
                 </div>
+
+                <div className="MatchSlider_Bar">
+                  <DashboardMatch_Slider/>
+                </div>
+
+                <div className="Match_detailsTab">
+                  <DashboardMatchDetail_Tab/>
+                </div>
+                
+                {/* table  */}
+
+                
+
                 <div class="games_images">
                   <div class="container">
                     <div class="row">
